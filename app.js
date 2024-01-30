0770ead96295eec3654600f691a0775e1791b21f
fulviavaz <fulviavaz@hotmail.com>
2024-01-30 17:10:14 -0300

    add mark all as read logic

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ btn.addEventListener("click", () => {
 // Clicking on the “Mark all as read” button will remove all the
 // notification items from the notifications list.
 
+const read = document.querySelector(".mark-as-read-btn");
+const list = document.querySelector(".notifications-list");
+
+read.addEventListener("click", () => {
+  list.innerHTML = "";
+})
+
 // Clicking on the “Decline” button will remove the “Decline” and “Accept”
 // buttons and will show a paragraph saying:
 // “The notification has been declined”.
